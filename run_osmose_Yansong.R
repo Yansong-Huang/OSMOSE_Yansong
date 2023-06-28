b768847efce0493b6068affbc653fca8c54b1ae0
Yansong Huang <yansong.huang@ifremer.com>
2023-06-28 12:21:51 +0200

    calibration files

--- run_osmose_Yansong.R
+++ run_osmose_Yansong.R
@@ -7,7 +7,7 @@ library(osmose)
 # library(ggplot2)
 library(calibrar)
 
-java_path   = "R:/sync/Github/osmose-private-ricardo/inst/java"
+#java_path   = "R:/sync/Github/osmose-private-ricardo/inst/java"
 java_path   = "C:/Users/yhuang/Documents/OSMOSE/osmose-private/inst/java"
 jar_file    = file.path(java_path, "osmose_4.4.0-jar-with-dependencies.jar")
 version    = "4.4.0"
@@ -15,16 +15,10 @@ version    = "4.4.0"
 config_dir  = "osmose-eec_v4.4_yansong"
 main_file = "eec_all-parameters.R"
 
-
 simulation = "Yansong"
 
 config_file = file.path(config_dir, main_file)
 output_dir  = file.path(config_dir, "output", simulation)
-
-# éxécuter le modèles
-# java_path <- "C:/Users/yhuang/Documents/OSMOSE/code/osmose_4.3.2.jar"
-
-# update_osmose(input=config_file, osmose = jar_file, version="4.4.0")
 
 conf = read_osmose(input=config_file)
 
@@ -36,14 +30,10 @@ out = initialize_osmose(input=config_file, type="internannual",
 
 run_osmose(input = config_file, osmose=jar_file, version = "4.4.0",
            output = output_dir)
-
-# lire les sorties
-output_osmose = read_osmose(path = output_dir)
-plot(output_osmose, what="yield")
+# calibration set-up
 
 calibration_path = osmose_calibration_setup(input=config_file, osmose=jar_file)
-# calibration_path = osmose_calibration_setup(input=config_file, osmose=jar_file, name = "Yansong_test", data_path = "../data")
-
+osmose_calibration_test(calibration_path)
 
 biomass = get_var(output_osmose, "biomass", expected=TRUE)
 class(biomass)
