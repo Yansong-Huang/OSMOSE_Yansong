2c7e0e74a098855b59cb39f64df632adbbcdf578
Yansong Huang <yansong.huang@ifremer.com>
2023-12-26 16:30:09 +0100

    update the parameters from calibration 12-21

--- read_calib_outputs.R
+++ read_calib_outputs.R
@@ -1,6 +1,6 @@
 #######################################################################
 # Source: Ghassen Halouani July 2020
-# Last modified: Yansong Huang 2023-12-12
+# Last modified: Yansong Huang 2023-12-21
 # Script to read calibration results 
 # and write new calibration-parameters.csv
 #######################################################################
@@ -11,7 +11,7 @@ library("dplyr")
 
 ## loading calibration results 
 calibration_results_test <- readRDS("calibration_simple_test3/osmose-test.results")
-calibration_results <- readRDS("osmose-Yansong-12-14.results")
+calibration_results <- readRDS("osmose-Yansong-12-21.results")
 
 opt_par = get_par(calibration_results$par, linear = TRUE)
 write_osmose(opt_par, file="calibration-parameters.csv")
@@ -31,11 +31,11 @@ g_fitness_phase4 <- apply(p_fitness_phase4, 1, sum)
 ## plot fitness
 g_fitness <- c(g_fitness_phase1, g_fitness_phase2, g_fitness_phase3, g_fitness_phase4)
 plot(g_fitness, type = "l", bty = "l", xlab = "Generations", ylab = "Fitness")
-abline(v=c(300,700,1200,1700), lty = c(2,2), col = c("grey", "grey"))
-text(300, 18000, "Phase 1")
-text(700, 16000, "Phase 2")
-text(1200, 14000, "Phase 3")
-text(1700, 12000, "Phase 4")
+abline(v=c(50,100,250,450), lty = c(2,2), col = c("grey", "grey"))
+text(50, 18000, "Phase 1")
+text(100, 16000, "Phase 2")
+text(250, 14000, "Phase 3")
+text(450, 12000, "Phase 4")
 
 
 
