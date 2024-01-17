42a523aa4115f44aac9e45b3bf749cd6d796f96f
Yansong Huang <yansong.huang@ifremer.com>
2024-01-17 09:46:55 +0100

    test 01-17

--- read_calib_outputs.R
+++ read_calib_outputs.R
@@ -1,6 +1,5 @@
 #######################################################################
 # Source: Ghassen Halouani July 2020
-# Last modified: Yansong Huang 2023-12-21
 # Script to read calibration results 
 # and write new calibration-parameters.csv
 #######################################################################
@@ -10,10 +9,14 @@ library("calibrar")
 library("dplyr")
 
 ## loading calibration results 
-calibration_results <- readRDS("osmose-Yansong-01-10.results")
+calibration_results <- readRDS("osmose-Yansong-01-12.results")
+partial_phase2 <- readRDS(".osmose-Yansong.partial.phase2")
+simulated_phase2 <- readRDS("osmose-Yansong-phase2.simulated")
 
 opt_par = get_par(calibration_results$par, linear = TRUE)
 write_osmose(opt_par, file="calibration-parameters.csv")
+# delete larval mortality deviation and initialisation biomass from the file
+
 
 ## partial fitness by generation
 p_fitness_phase1 <- calibration_results$phases[[1]]$trace$fitness
@@ -28,13 +31,12 @@ g_fitness_phase3 <- apply(p_fitness_phase3, 1, sum)
 g_fitness_phase4 <- apply(p_fitness_phase4, 1, sum)
 
 ## plot fitness
-g_fitness <- c(g_fitness_phase1, g_fitness_phase2, g_fitness_phase3, g_fitness_phase4)
+g_fitness <- c(g_fitness_phase1, g_fitness_phase2, g_fitness_phase3)
 plot(g_fitness, type = "l", bty = "l", xlab = "Generations", ylab = "Fitness")
-abline(v=c(200,400,600,800), lty = c(2,2), col = c("grey", "grey"))
+abline(v=c(200,400,600), lty = c(2,2), col = c("grey", "grey"))
 text(100, 60000, "Phase 1")
 text(300, 60000, "Phase 2")
 text(500, 60000, "Phase 3")
-text(700, 60000, "Phase 4")
 
 # update catchability matrix
 # update object conf in run_osmose script
