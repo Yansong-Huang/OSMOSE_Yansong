e0d4c38ea7b0fbf06f370c7796e2d8bf5c89c1e6
Yansong Huang <yansong.huang@ifremer.com>
2024-02-02 18:03:17 +0100

    modified some growth parameters of herring, sardine, poor cod

--- read_calib_outputs.R
+++ read_calib_outputs.R
@@ -9,7 +9,11 @@ library("calibrar")
 library("dplyr")
 
 ## loading calibration results 
+<<<<<<< HEAD
 calibration_results <- readRDS("osmose-Yansong-01-29.results")
+=======
+calibration_results <- readRDS("osmose-Yansong-01-24-3.results")
+>>>>>>> b37fc95ae38c9550c0156ad2eabd5b73cefb0ab5
 
 opt_par = get_par(calibration_results$par, linear = TRUE)
 write_osmose(opt_par, file="calibration-parameters.csv")
@@ -29,10 +33,17 @@ g_fitness_phase3 <- apply(p_fitness_phase3, 1, sum)
 ## plot fitness
 g_fitness <- c(g_fitness_phase1, g_fitness_phase2, g_fitness_phase3)
 plot(g_fitness, type = "l", bty = "l", xlab = "Generations", ylab = "Fitness")
+<<<<<<< HEAD
 abline(v=c(200,400,800), lty = c(2,2), col = c("grey", "grey","grey"))
 text(100, 90000, "Phase 1")
 text(300, 90000, "Phase 2")
 text(600, 90000, "Phase 3")
+=======
+abline(v=c(200,400,600), lty = c(2,2), col = c("grey", "grey","grey"))
+text(100, 90000, "Phase 1")
+text(300, 90000, "Phase 2")
+text(500, 90000, "Phase 3")
+>>>>>>> b37fc95ae38c9550c0156ad2eabd5b73cefb0ab5
 
 # update catchability matrix
 # update object conf in run_osmose script
