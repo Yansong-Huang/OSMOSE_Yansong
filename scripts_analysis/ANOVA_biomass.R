f9f995709170a1fc0b2399f1db31e75a43ab1d62
Yansong Huang <yansong.huang@ifremer.com>
2024-11-21 15:59:50 +0100

    add ANOVA scripts

---
+++ scripts_analysis/ANOVA_biomass.R
@@ -0,0 +1,85 @@
+# Boxplot of total biomass
+# Auteur : Yansong Huang
+# Date de création : 2024-10-31
+
+library(tidyr)
+library(dplyr)
+library(purrr)
+
+# variables globales
+deployment_scenarios <- c("cout","protection","loin","equilibre")
+regulation_scenarios <- c("sans_fermeture","fermeture_chalut","fermeture_totale")
+CC_scenarios <- c("ON","OFF")
+n_years_cut <- c(10,21,22,34,35,49)
+n_replicate <- 30
+
+process_biomass <- function(current_results_path,cut_off_year_begin, cut_off_year_end) {
+  list_biomass_base <- list.files(results_path_base, "Yansong_biomass_Simu.*csv", full.names = TRUE)
+  list_biomass_current <- list.files(current_results_path, "Yansong_biomass_Simu.*csv", full.names = TRUE)
+  
+  biomass_relative <- lapply(1:n_replicate, function(simulation) {
+    biomass_brut_base <- read.csv(list_biomass_base[simulation], skip = 1)
+    biomass_brut_current <- read.csv(list_biomass_current[simulation], skip = 1)
+    biomass_total_base <- biomass_brut_base %>% 
+      filter(Time>cut_off_year_begin)%>%
+      filter(Time<cut_off_year_end)%>% 
+      colMeans() %>% sum()
+    biomass_total_current <- biomass_brut_current %>% 
+      filter(Time>cut_off_year_begin)%>%
+      filter(Time<cut_off_year_end)%>% 
+      colMeans() %>% sum()
+    biomass_ratio <- biomass_total_current/biomass_total_base
+  })
+  biomass_relative <- as.vector(biomass_relative) %>% as.numeric()
+  return(biomass_relative)
+}
+
+# 初始化全局数据框
+total_biomass_all <- data.frame()
+
+# 遍历每个捕鱼政策情境
+for (regulation in regulation_scenarios) {
+  
+  # 构建场景路径
+  results_path_1 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[1], "_", regulation), "Base", "output", "CIEM")
+  results_path_2 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[2], "_", regulation), "Base", "output", "CIEM")
+  results_path_3 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[3], "_", regulation), "Base", "output", "CIEM")
+  results_path_4 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[4], "_", regulation), "Base", "output", "CIEM")
+  
+  results_path_scenario <- list(results_path_1, results_path_2, results_path_3, results_path_4)
+  results_path_base <- file.path("outputs/results_1111", "Base_simu", "Base", "output", "CIEM")
+  
+  # 分别计算三个时间段的数据
+  
+  total_biomass_after_list <- map(results_path_scenario, ~ process_biomass(
+    current_results_path = .x,
+    cut_off_year_begin = n_years_cut[5],
+    cut_off_year_end = n_years_cut[6]
+  ))
+  
+  names(total_biomass_after_list) <- c("cost", "protection", "distance", "balance")
+  
+  # 转换为数据框并添加标识
+  total_biomass_after_table <- stack(total_biomass_after_list) %>%
+    mutate(regulation = regulation)
+  
+  # 合并到全局数据框
+  total_biomass_all <- rbind(
+    total_biomass_all,
+    total_biomass_after_table
+  )
+}
+colnames(total_biomass_all) <- c("biomass_ratio", "deployment","regulation")
+
+total_biomass_all$regulation <- factor(
+  total_biomass_all$regulation,
+  levels = c("sans_fermeture", "fermeture_chalut", "fermeture_totale"),
+  labels = c("no closure", "trawlers closure", "complete closure")
+)
+
+# Levene test 
+leveneTest(biomass_ratio ~ regulation * deployment, data = total_biomass_all)
+
+#ANOVa
+anova_model <- aov(biomass_ratio ~ regulation * deployment, data = total_biomass_all)
+summary(anova_model)
---
+++ scripts_analysis/ANOVA_yield.R
@@ -0,0 +1,85 @@
+# Boxplot of total yield
+# Auteur : Yansong Huang
+# Date de création : 2024-10-31
+
+library(tidyr)
+library(dplyr)
+library(purrr)
+
+# variables globales
+deployment_scenarios <- c("cout","protection","loin","equilibre")
+regulation_scenarios <- c("sans_fermeture","fermeture_chalut","fermeture_totale")
+CC_scenarios <- c("ON","OFF")
+n_years_cut <- c(10,21,22,34,35,49)
+n_replicate <- 30
+
+process_yield <- function(current_results_path,cut_off_year_begin, cut_off_year_end) {
+  list_yield_base <- list.files(results_path_base, "Yansong_yield_Simu.*csv", full.names = TRUE)
+  list_yield_current <- list.files(current_results_path, "Yansong_yield_Simu.*csv", full.names = TRUE)
+  
+  yield_relative <- lapply(1:n_replicate, function(simulation) {
+    yield_brut_base <- read.csv(list_yield_base[simulation], skip = 1)
+    yield_brut_current <- read.csv(list_yield_current[simulation], skip = 1)
+    yield_total_base <- yield_brut_base %>% 
+      filter(Time>cut_off_year_begin)%>%
+      filter(Time<cut_off_year_end)%>% 
+      colMeans() %>% sum()
+    yield_total_current <- yield_brut_current %>% 
+      filter(Time>cut_off_year_begin)%>%
+      filter(Time<cut_off_year_end)%>% 
+      colMeans() %>% sum()
+    yield_ratio <- yield_total_current/yield_total_base
+  })
+  yield_relative <- as.vector(yield_relative) %>% as.numeric()
+  return(yield_relative)
+}
+
+# 初始化全局数据框
+total_yield_all <- data.frame()
+
+# 遍历每个捕鱼政策情境
+for (regulation in regulation_scenarios) {
+  
+  # 构建场景路径
+  results_path_1 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[1], "_", regulation), "Base", "output", "CIEM")
+  results_path_2 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[2], "_", regulation), "Base", "output", "CIEM")
+  results_path_3 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[3], "_", regulation), "Base", "output", "CIEM")
+  results_path_4 <- file.path("outputs/results_1111", paste0("CC.", CC_scenarios[1], "_", deployment_scenarios[4], "_", regulation), "Base", "output", "CIEM")
+  
+  results_path_scenario <- list(results_path_1, results_path_2, results_path_3, results_path_4)
+  results_path_base <- file.path("outputs/results_1111", "Base_simu", "Base", "output", "CIEM")
+  
+  # 分别计算三个时间段的数据
+  
+  total_yield_after_list <- map(results_path_scenario, ~ process_yield(
+    current_results_path = .x,
+    cut_off_year_begin = n_years_cut[5],
+    cut_off_year_end = n_years_cut[6]
+  ))
+
+  names(total_yield_after_list) <- c("cost", "protection", "distance", "balance")
+  
+  # 转换为数据框并添加标识
+  total_yield_after_table <- stack(total_yield_after_list) %>%
+    mutate(regulation = regulation)
+  
+  # 合并到全局数据框
+  total_yield_all <- rbind(
+    total_yield_all,
+    total_yield_after_table
+  )
+}
+colnames(total_yield_all) <- c("yield_ratio", "deployment","regulation")
+
+total_yield_all$regulation <- factor(
+  total_yield_all$regulation,
+  levels = c("sans_fermeture", "fermeture_chalut", "fermeture_totale"),
+  labels = c("no closure", "trawlers closure", "complete closure")
+)
+
+# Levene test 
+leveneTest(yield_ratio ~ regulation * deployment, data = total_yield_all)
+
+#ANOVa
+anova_model <- aov(yield_ratio ~ regulation * deployment, data = total_yield_all)
+summary(anova_model)
