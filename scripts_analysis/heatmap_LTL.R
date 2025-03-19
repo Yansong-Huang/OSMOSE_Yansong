bd13ed225c760210e3e3de5d06db12ae0308ff1c
Yansong Huang <yansong.huang@ifremer.com>
2025-03-19 11:58:22 +0100

    calculate annual average of 2049

--- scripts_analysis/heatmap_LTL.R
+++ scripts_analysis/heatmap_LTL.R
@@ -15,7 +15,7 @@ nc_ERSEM <- nc_open("osmose-eec_v4.4_yansong/Base/input/ERSEM_nc/interpolated_CE
 LTL_names <- c("depositBenthos","suspensionBenthos","meioBenthos","diatoms",
                "microPhytoplankton","mesoZooplankton","microZooplankton","heterotrophicFlagellates")
 
-diatoms <- ncvar_get(nc_ERSEM, varid = "diatoms")
+diatom <- ncvar_get(nc_ERSEM, varid = "diatoms")
 micro_phytoplankton <- ncvar_get(nc_ERSEM, varid = "microPhytoplankton")
 meso_zooplankton <- ncvar_get(nc_ERSEM, varid = "mesoZooplankton")
 micro_zooplankton <- ncvar_get(nc_ERSEM, varid = "microZooplankton")
@@ -24,6 +24,25 @@ meio_benthos <- ncvar_get(nc_ERSEM, varid = "meioBenthos")
 suspension_feeder <- ncvar_get(nc_ERSEM, varid = "suspensionBenthos")
 deposit_feeder <- ncvar_get(nc_ERSEM, varid = "depositBenthos")
 
+# calculate mean value of the last year
+diatom_subset <- diatom[,,457:480]
+micro_phytoplankton_subset <- micro_phytoplankton[,,457:480]
+meso_zooplankton_subset <- meso_zooplankton[,,457:480]
+micro_zooplankton_subset <- micro_zooplankton[,,457:480]
+heterotrophic_flagellates_subset <- heterotrophic_flagellates[,,457:480]
+meio_benthos_subset <- meio_benthos[,,457:480]
+suspension_feeder_subset <- suspension_feeder[,,457:480]
+deposit_feeder_subset <- deposit_feeder[,,457:480]
+
+diatom_mean <- rowMeans(diatom_subset, dims = 2) # dims = 2 means keeping the first two dimensions
+micro_phytoplankton_mean <- rowMeans(micro_phytoplankton_subset, dims=2)
+meso_zooplankton_mean <- rowMeans(meso_zooplankton_subset, dims=2)
+micro_zooplankton_mean <- rowMeans(micro_zooplankton_subset, dims=2)
+heterotrophic_flagellates_mean <- rowMeans(heterotrophic_flagellates_subset, dims=2)
+meio_benthos_mean <- rowMeans(meio_benthos_subset, dims = 2)
+suspension_feeder_mean <- rowMeans(suspension_feeder_subset, dims = 2)
+deposit_feeder_mean <- rowMeans(deposit_feeder_subset, dims = 2)
+
 nc_close(nc_ERSEM)
 # set longitude and latitude
 lon <- seq(-1.95,2.45,by=0.1)
@@ -31,14 +50,14 @@ lat <- seq(49.05,51.15,by=0.1)
 
 # Prepare grid and mask
 map_grid <- expand.grid(lon = lon, lat = lat)
-map_grid$diatoms <- as.vector(diatoms[,,480])
-map_grid$micro_phytoplanktons <- as.vector(micro_phytoplankton[,,480])
-map_grid$meso_zooplanktons <- as.vector(meso_zooplankton[,,480])
-map_grid$micro_zooplanktons <- as.vector(micro_zooplankton[,,480])
-map_grid$heterotrophic_flagellates <- as.vector(heterotrophic_flagellates[,,480])
-map_grid$meio_benthos <- as.vector(meio_benthos[,,480])
-map_grid$suspension_feeders <- as.vector(suspension_feeder[,,480])
-map_grid$deposit_feeders <- as.vector(deposit_feeder[,,480])
+map_grid$diatoms <- as.vector(diatom_mean)
+map_grid$micro_phytoplanktons <- as.vector(micro_phytoplankton_mean)
+map_grid$meso_zooplanktons <- as.vector(meso_zooplankton_mean)
+map_grid$micro_zooplanktons <- as.vector(micro_zooplankton_mean)
+map_grid$heterotrophic_flagellates <- as.vector(heterotrophic_flagellates_mean)
+map_grid$meio_benthos <- as.vector(meio_benthos_mean)
+map_grid$suspension_feeders <- as.vector(suspension_feeder_mean)
+map_grid$deposit_feeders <- as.vector(deposit_feeder_mean)
 
 # Filter to cut off west of Cotentin
 map_grid_cut <- filter(map_grid, lon > -1.6)
@@ -60,7 +79,7 @@ map_diatoms <- ggplot(map_grid_cut[,c(1,2,3)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/diatoms_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/diatoms_2049.png",
        map_diatoms, width = 4.5, height = 2.5, dpi = 600)
   
 map_micro_phytoplankton <- ggplot(map_grid_cut[,c(1,2,4)]) +
@@ -77,7 +96,7 @@ map_micro_phytoplankton <- ggplot(map_grid_cut[,c(1,2,4)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/micro_phytoplankton_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/micro_phytoplankton_2049.png",
        map_micro_phytoplankton, width = 4.5, height = 2.5, dpi = 600)
 
 map_meso_zooplankton <- ggplot(map_grid_cut[,c(1,2,5)]) +
@@ -94,7 +113,7 @@ map_meso_zooplankton <- ggplot(map_grid_cut[,c(1,2,5)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/meso_zooplankton_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/meso_zooplankton_2049.png",
        map_meso_zooplankton, width = 4.5, height = 2.5, dpi = 600)
 
 map_micro_zooplankton <- ggplot(map_grid_cut[,c(1,2,6)]) +
@@ -111,7 +130,7 @@ map_micro_zooplankton <- ggplot(map_grid_cut[,c(1,2,6)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/micro_zooplankton_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/micro_zooplankton_2049.png",
        map_micro_zooplankton, width = 4.5, height = 2.5, dpi = 600)
 
 map_heterotrophic_flagellates <- ggplot(map_grid_cut[,c(1,2,7)]) +
@@ -128,7 +147,7 @@ map_heterotrophic_flagellates <- ggplot(map_grid_cut[,c(1,2,7)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/heterotrophic_flagellates_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/heterotrophic_flagellates_2049.png",
        map_heterotrophic_flagellates, width = 4.5, height = 2.5, dpi = 600)
 
 map_meio_benthos <- ggplot(map_grid_cut[,c(1,2,8)]) +
@@ -145,7 +164,7 @@ map_meio_benthos <- ggplot(map_grid_cut[,c(1,2,8)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/meio_benthos_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/meio_benthos_2049.png",
        map_meio_benthos, width = 4.5, height = 2.5, dpi = 600)
 
 map_suspension_feeders <- ggplot(map_grid_cut[,c(1,2,9)]) +
@@ -162,7 +181,7 @@ map_suspension_feeders <- ggplot(map_grid_cut[,c(1,2,9)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/suspension_feeders_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/suspension_feeders_2049.png",
        map_suspension_feeders, width = 4.5, height = 2.5, dpi = 600)
 
 map_deposit_feeders <- ggplot(map_grid_cut[,c(1,2,10)]) +
@@ -179,5 +198,5 @@ map_deposit_feeders <- ggplot(map_grid_cut[,c(1,2,10)]) +
     legend.text = element_text(size = 8),
   )
 
-ggsave("figures/publication/LTL_heatmap/deposit_feeders_2049_12.png",
+ggsave("figures/publication/LTL_heatmap/deposit_feeders_2049.png",
        map_deposit_feeders, width = 4.5, height = 2.5, dpi = 600)
